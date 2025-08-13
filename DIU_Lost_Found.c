6d9394f8cc539417ef9a39c02392c345b08e673f
Sajib Kundu <sajibkundu.swe@gmail.com>
2025-08-14 02:55:10 +0600

    Update DIU_Lost_Found.c

--- DIU_Lost_Found.c
+++ DIU_Lost_Found.c
@@ -1,1 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+#define MAX_CATEGORIES 4
+#define MAX_NAME_LEN 50
+#define MAX_ID_LEN 15
+#define MAX_PHONE_LEN 15
+#define MAX_QUEUE_SIZE 50
+#define MAX_STACK_SIZE 50
